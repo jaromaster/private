45ff13d3ca0df02fd9f1f40f83b1b1069c221525
Jaromir Blutaumüller <jaromirblu@gmail.com>
2023-01-07 11:41:35 +0100

    basic folder encryption

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use constants::constants::Actions;
 use crypt::crypt::action_on_file;
 
 use crate::parse::parse::parse_args;
@@ -19,10 +20,36 @@ fn main() {
 
     // execute action on targets (e.g. encrypt or decrypt)
     for target in targets {
-        let err_option = action_on_file(&action, &target, &key);
-        if err_option.is_some() {
-            eprintln!("{}", err_option.unwrap());
-            std::process::exit(1);
+        let target_path = std::path::Path::new(&target);
+        if target_path.is_file() {
+            handle_file(&action, &target, &key);
         }
+        else {
+            println!("encountered dir: {}", target);
+            handle_dir(&action, &target, &key);
+        }
+    }
+}
+
+
+/// execute action, on files only
+fn handle_file(action: &Actions, file_path: &str, key: &str) {
+    let err_option = action_on_file(&action, &file_path, &key);
+    if err_option.is_some() {
+        eprintln!("{}", err_option.unwrap());
+        std::process::exit(1);
+    }
+}
+
+/// execute action on all files in directory (recursively)
+fn handle_dir(action: &Actions, dir_path: &str, key: &str) {
+    for entry in std::fs::read_dir(dir_path).unwrap() {
+        let entry = entry.unwrap();
+
+        if entry.path().is_dir() {
+            handle_dir(action, entry.path().to_str().unwrap(), key);
+            continue;
+        }
+        handle_file(action, entry.path().to_str().unwrap(), key);
     }
 }
